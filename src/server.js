40ed4da4f4f882e0b07235ec0ac3dd8c51ad7ba3
Jorge Arias <94802496+joariasdev@users.noreply.github.com>
2025-10-16 20:50:53 -0400

    feat: connect server and database

--- src/server.js
+++ src/server.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const app = express();
+const pool = require('./db');
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 2500;
 
 app.get('/', (req, res) => {
     res.send("Issue Tracker")
 });
+
+app.get('/issues', async (req, res) => {
+    const result = await pool.query('SELECT * FROM issues')
+    res.json(result.rows);
+})
 
 app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`));
